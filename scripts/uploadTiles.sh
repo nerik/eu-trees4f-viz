d21c7ba9d8c48f7bfcf663dd8af0ec6ac233b094
Erik Escoffier <me@erik.nz>
2023-04-04 14:48:07 +0200

    Removed content and set up redirection

--- scripts/uploadTiles.sh
+++
@@ -1,3 +0,0 @@
-gsutil -o GSUtil:parallel_process_count=1 -m  cp -r ./out/pbf gs://eu-trees4f-tiles
-gsutil cors set ./scripts/CORS.json gs://eu-trees4f-tiles
-gsutil -o GSUtil:parallel_process_count=1 -m setmeta -r -h "Content-Encoding:gzip" gs://eu-trees4f-tiles/pbf
